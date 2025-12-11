ca2148ec0cb3427806140dd4d07c341e80ce28e0
Shivamahajan045 <111225580+Shivamahajan045@users.noreply.github.com>
2025-12-11 16:37:47 +0530

    digital clock

---
+++ digital-clock/src/DigitalClock.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+const DigitalClock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const formatTime = () => {
+    let hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+
+    hours = hours % 12 || 12;
+
+    return `${padZeroes(hours)}: ${padZeroes(minutes)}: ${padZeroes(
+      seconds
+    )}: ${padZeroes(meridiem)}`;
+  };
+
+  const padZeroes = (number) => {
+    return (number < 10 ? "0" : "") + number;
+  };
+  return (
+    <div>
+      <div className="clock-container">
+        <div className="clock">
+          <span>{formatTime()}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DigitalClock;
