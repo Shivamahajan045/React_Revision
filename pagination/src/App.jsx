51492b18d309fff9f74cf7e6d52b4e0d6b8d9b82
Shivamahajan045 <111225580+Shivamahajan045@users.noreply.github.com>
2025-12-20 16:37:04 +0530

    pagination

---
+++ pagination/src/App.jsx
@@ -0,0 +1,11 @@
+import Pagination from "./components/Pagination";
+function App() {
+  return (
+    <div className="app">
+      <h1>Learning pagination</h1>
+      <Pagination />
+    </div>
+  );
+}
+
+export default App;
